c65fc9be4961765810396cfcf9620f5337d3be36
dediwibisono17 <dedi.wibisono17@gmail.com>
2022-10-06 14:28:43 +0700

    update

--- js/app1.js
+++ js/app1.js
@@ -2,12 +2,14 @@
      $(document).ready(function() {
       new WOW().init();
       var $isAnimatedSecond = $('.pemicu .is-animated');
-      var $isAnimatedFour = $('.four .is-animated');
+      var $isAnimatedOne = $('.one .is-animated');
+      var $isAnimatedDaftar = $('.daftar .is-animated');
         $('#fullpage').fullpage({
           onLeave: function(index,nextIndex, direction){
             // animate__animated animate__fadeIn" data-wow-delay="0.5s"
              // first animation
                 if( index == 1 && nextIndex == 2 ) { 
+                  $isAnimatedOne.eq(0).addClass('animate__animated animate__fadeIn animate__delay-2s	2s')
                   //$isAnimatedSecond.eq(0).css('animation-delay', '.3s');
                   // $isAnimatedSecond.eq(1).css('animation-delay', '.6s');
                   // $isAnimatedSecond.eq(2).css('animation-delay', '.9s');
@@ -15,11 +17,12 @@
                 }
                 
                 else if( ( index == 1 || index == 2 ) && nextIndex == 3 ) {
-                  $isAnimatedSecond.eq(0).addClass('animate__animated animate__fadeInRight animate__delay-1s	1s').attr("data-wow-delay","2.5s"); 
-                  $isAnimatedSecond.eq(1).addClass('animate__animated animate__fadeInLeft animate__delay-1s	1s').attr("data-wow-delay","3.5s"); 
-                  $isAnimatedSecond.eq(2).addClass('animate__animated animate__fadeInRight animate__delay-2s	2s').attr("data-wow-delay","2.5s"); 
-                  $isAnimatedSecond.eq(3).addClass('animate__animated animate__fadeInLeft animate__delay-2s	2s').attr("data-wow-delay","3.5s"); 
-                    // $isAnimatedThird.addClass('animate__animated animate__fadeInLeft animate__repeat-2	2').attr("data-wow-delay","2.5s"); 
+                  $isAnimatedSecond.eq(0).addClass('animate__animated animate__fadeIn animate__delay-1s	1s');
+                  $isAnimatedSecond.eq(1).addClass('animate__animated animate__fadeInRight animate__delay-2s	2s')
+                  $isAnimatedSecond.eq(2).addClass('animate__animated animate__fadeInLeft animate__delay-2s	2s')
+                  $isAnimatedSecond.eq(3).addClass('animate__animated animate__fadeInRight animate__delay-3s	3s')
+                  $isAnimatedSecond.eq(4).addClass('animate__animated animate__fadeInLeft animate__delay-3s	3s')
+                    // $isAnimatedThird.addClass('animate__animated animate__fadeInLeft animate__repeat-2	2')
                     // $isAnimatedThird.eq(1).addClass('animated fadeInLeftBig').css('animation-delay', '.6s');
                     // $isAnimatedThirdSingle.addClass('animated bounceInDown').css('animation-delay', '1.2s');
                 }
@@ -27,6 +30,14 @@
            
                 // third animation
                 else if( ( index == 1 || index == 2 || index == 3 ) && nextIndex == 4 ) {
+                  $isAnimatedDaftar.eq(0).addClass('animate__animated animate__fadeIn animate__delay-1s	1s');
+                  $isAnimatedDaftar.eq(1).addClass('animate__animated animate__fadeInLeft animate__delay-2s	2s');
+                  $isAnimatedDaftar.eq(2).addClass('animate__animated animate__fadeInLeft animate__delay-2s	2s');
+                  $isAnimatedDaftar.eq(3).addClass('animate__animated animate__fadeInRight animate__delay-3s	3s');
+                  $isAnimatedDaftar.eq(4).addClass('animate__animated animate__fadeInRight animate__delay-3s	3s');
+                  $isAnimatedDaftar.eq(5).addClass('animate__animated animate__fadeIn animate__delay-4s	4s');
+                  $isAnimatedDaftar.eq(6).addClass('animate__animated animate__fadeIn animate__delay-4s	4s');
+                  
                     // $isAnimatedFour.eq(0).addClass('animate__animated animate__fadeInRight').attr("data-wow-delay","2.5s");
                     // $isAnimatedFour.eq(1).addClass('animate__animated animate__fadeInLeft').attr("data-wow-delay","3.8s");;
                     // $isAnimatedFourth.addClass('animated zoomIn').css('animation-delay', '.6s');
